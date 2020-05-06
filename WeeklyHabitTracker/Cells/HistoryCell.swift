3f5d4edc3cd2e7ac91dc1594f3302349170882d1
Robert Parsons <robertparsons4@icloud.com>
2020-05-06 12:56:03 -0700

    Added HistoryCell to repository.

---
+++ WeeklyHabitTracker/Cells/HistoryCell.swift
@@ -0,0 +1,46 @@
+//
+//  HIstoryCell.swift
+//  WeeklyHabitTracker
+//
+//  Created by Robert Parsons on 5/5/20.
+//  Copyright © 2020 Robert Parsons. All rights reserved.
+//
+
+import UIKit
+
+class HistoryCell: UICollectionViewCell {
+    let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 14
+        clipsToBounds = true
+        
+        configureTitleLabel()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureTitleLabel() {
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = .white
+        titleLabel.numberOfLines = 0
+    }
+    
+    func configureConstraints() {
+        addSubview(titleLabel)
+        titleLabel.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: 0, height: 0)
+    }
+    
+    func setTitleLabelText(_ title: String) {
+        titleLabel.text = title
+    }
+    
+    func setBackgroundColor(_ color: UIColor) {
+        backgroundColor = color
+    }
+}
