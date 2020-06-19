2b5aa18cc1b86d1027c56ee7784b41a2ceacb250
Robert Parsons <robertparsons4@icloud.com>
2020-06-19 12:48:37 -0700

    Refactored HistoryTitleCell to include the percentage.

--- WeeklyHabitTracker/Cells/History/HistoryTitleCell.swift
+++ WeeklyHabitTracker/Cells/History/HistoryTitleCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HistoryTitleCell: UICollectionViewCell {
     let titleLabel = UILabel()
+    let percentLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -17,6 +18,7 @@ class HistoryTitleCell: UICollectionViewCell {
         clipsToBounds = true
         
         configureTitleLabel()
+        configurePercentLabel()
         configureConstraints()
     }
     
@@ -25,19 +27,36 @@ class HistoryTitleCell: UICollectionViewCell {
     }
     
     func configureTitleLabel() {
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        titleLabel.textAlignment = .center
+//        titleLabel.backgroundColor = .systemPink
+        titleLabel.font = UIFont.systemFont(ofSize: 17)
+        titleLabel.textAlignment = .left
         titleLabel.textColor = .white
         titleLabel.numberOfLines = 0
+        titleLabel.lineBreakMode = .byWordWrapping
+    }
+    
+    func configurePercentLabel() {
+//        percentLabel.backgroundColor = .black
+        percentLabel.font = UIFont.systemFont(ofSize: 28, weight: .heavy)
+        percentLabel.textAlignment = .left
+        percentLabel.textColor = .white
+        percentLabel.numberOfLines = 0
+        percentLabel.text = "100%"
     }
     
     func configureConstraints() {
+        addSubview(percentLabel)
+        percentLabel.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 5, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: 0, height: 45)
         addSubview(titleLabel)
-        titleLabel.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: 0, height: 0)
+        titleLabel.anchor(top: percentLabel.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 5, paddingRight: 10, width: 0, height: 0)
     }
     
     func setTitleLabelText(_ title: String) {
         titleLabel.text = title
+    }
+    
+    func setPercentLabelText(_ title: String) {
+        percentLabel.text = title
     }
     
     func setBackgroundColor(_ color: UIColor) {
