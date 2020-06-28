9cf73125c534243e890c9cd35a5a7cc8521badc3
Robert Parsons <robertparsons4@icloud.com>
2020-06-27 17:02:38 -0700

    Changed cell heights to 90.

--- WeeklyHabitTracker/Screens/Home/HomeCollectionViewController.swift
+++ WeeklyHabitTracker/Screens/Home/HomeCollectionViewController.swift
@@ -70,7 +70,7 @@ class HomeCollectionViewController: UICollectionViewController, UICollectionView
     }
         
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width - 30, height: 85)
+        return CGSize(width: view.frame.width - 30, height: 90)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
