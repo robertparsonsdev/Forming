e2ac95361f45c099da53bd4d655b44d8c470a6e8
Robert Parsons <robertparsons4@icloud.com>
2020-07-07 21:14:21 -0700

    Got very basic three button and list view set up for Settings.

--- WeeklyHabitTracker/Screens/Settings/SettingsSwiftUI.swift
+++ WeeklyHabitTracker/Screens/Settings/SettingsSwiftUI.swift
@@ -10,12 +10,43 @@ import SwiftUI
 
 struct SettingsSwiftUI: View {
     var body: some View {
-        Text("This is a test of SwiftUI.")
+        NavigationView {
+            VStack(spacing: 10) {
+                TipButton(buttonNumber: 1)
+                TipButton(buttonNumber: 2)
+                TipButton(buttonNumber: 3)
+                SettingsList()
+            }.navigationBarTitle(Text("Settings"))
+        }
     }
 }
 
 struct SettingsSwiftUI_Previews: PreviewProvider {
     static var previews: some View {
         SettingsSwiftUI()
+    }
+}
+
+struct TipButton: View {
+    let buttonNumber: Int
+    var body: some View {
+        Button(action: {
+            print(self.buttonNumber)
+        }) {
+            Text(String(buttonNumber))
+                .padding()
+                .padding()
+                .background(Color(.tertiarySystemFill))
+                .cornerRadius(14)
+        }
+    }
+}
+
+struct SettingsList: View {
+    var body: some View {
+        List {
+            Text("First Row")
+        }.listStyle(GroupedListStyle())
+            .environment(\.horizontalSizeClass, .regular)
     }
 }
